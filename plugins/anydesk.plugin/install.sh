9b7e3f9bdc70e433e46c1d504241be43e45c09c0
89luca89 <luca.dimaio1@gmail.com>
2017-09-01 10:06:11 +0200

    [WIP] Expand Software Offering: Anydesk

---
+++ plugins/anydesk.plugin/install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+arch | grep x86_64
+VERSION=$(wget https://anydesk.com/download -O - | grep -e "v.[0-9].[0-9].[0-9]" | head -n 1 | sed 's/<small>//' | sed 's/<\/small><br>//' | sed 's/ //g' | sed 's/\t//g')
+if (( $? == 0));then
+	# 64 bit
+	URL="https://download.anydesk.com/linux/rhel7/anydesk-"$VERSION"-1.el7.x86_64.rpm"
+else
+	# 32 bit 
+	URL="https://download.anydesk.com/linux/rhel7/anydesk-"$VERSION"-1.el7.i686.rpm"
+fi
+wget $URL -O "/var/cache/fedy/anydesk.rpm"
+dnf -y install "/var/cache/fedy/anydesk.rpm"
