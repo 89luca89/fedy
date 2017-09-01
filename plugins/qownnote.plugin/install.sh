3a636233b86a03ea5216fafba6d87f045631772c
89luca89 <luca.dimaio1@gmail.com>
2017-09-01 11:49:45 +0200

    [WIP] Expand Software Offering: QOwnNote

---
+++ plugins/qownnote.plugin/install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+rpm --import http://download.opensuse.org/repositories/home:/pbek:/QOwnNotes/Fedora_25/repodata/repomd.xml.key
+wget http://download.opensuse.org/repositories/home:/pbek:/QOwnNotes/Fedora_25/home:pbek:QOwnNotes.repo -O /etc/yum.repos.d/qownnotes.repo
+dnf -y install qownnotes 
