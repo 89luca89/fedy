60597cd63a238979d267ef6a25a6b2fd21a65d6f
89luca89 <luca.dimaio1@gmail.com>
2017-09-01 10:31:58 +0200

    Fix Eclipse Install

--- plugins/eclipsestandard.plugin/install.sh
+++ plugins/eclipsestandard.plugin/install.sh
@@ -10,14 +10,15 @@ INSTALLDIR="/opt/eclipse-$PACKAGE"
 mkdir -p "$CACHEDIR"
 cd "$CACHEDIR"
 
-BASEURL="http://mirror.cc.vt.edu/pub/eclipse/technology/epp/downloads/release"
-PRESENTRELEASE=$(wget $BASEURL/release.xml -O - | grep present | sed -n 's:.*<present>\(.*\)</present>.*:\1:p')
-PRODUCT=$(wget --quiet $BASEURL/$PRESENTRELEASE/$PACKAGE.xml -O - | grep "<product name=" | sed 's/.*"\(.*\)"[^"]*$/\1/')
+	BASEURL="http://mirror.cc.vt.edu/pub/eclipse/technology/epp/downloads/release"
+	PRESENTRELEASE=$(wget $BASEURL/release.xml -O - | grep present | sed -n 's:.*<present>\(.*\)</present>.*:\1:p')
+	PRODUCT=$(echo $PRESENTRELEASE | sed 's/\//-/')
+
 
 if [[ $(uname -m) == 'x86_64' ]]; then
-	URL=$BASEURL/$PRESENTRELEASE/$PRODUCT-linux-gtk-x86_64.tar.gz
+	URL=$BASEURL/$PRESENTRELEASE/eclipse-java-$PRODUCT-linux-gtk-x86_64.tar.gz
 else
-	URL=$BASEURL/$PRESENTRELEASE/$PRODUCT-linux-gtk.tar.gz
+	URL=$BASEURL/$PRESENTRELEASE/eclipse-java-$PRODUCT-linux-gtk.tar.gz
 fi
 
 FILE=${URL##*/}
